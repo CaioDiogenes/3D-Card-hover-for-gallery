e8fa5c6b4345ff8973557852eedb76c88cba0f7f
Corchc <kaitohiraga2015@gmail.com>
2025-02-19 21:09:37 -0300

    Updates

--- projects/my-library/README.md
+++ projects/my-library/README.md
@@ -1,19 +1,27 @@
 # Usage
 
-Selector usage is <lib-figure-card [imgs]="imgs"></lib-figure-card> and you need to pass you img array as the follow example:
+Selector usage is:
+```<lib-figure-card [imgs]="imgs"></lib-figure-card>```
+and you need to pass your "img" input array as the follow example:
+
 ```
 imgs = [
+    // okarun
     {
-      object3D: "https://www.imagensempng.com.br/wp-content/uploads/2022/02/0424-5.png",
-      title: "https://banner2.cleanpng.com/20180513/ggq/kisspng-logo-brand-font-5af8f9c45b0e25.898966601526266308373.jpg",
-      cover: "https://i.pinimg.com/736x/08/c9/1d/08c91da46d9608ac878835c73c4e9cf0.jpg",
+      object3D: "https://i.imgur.com/vWa25XP.gif",
+      title: "https://tamashiiweb.com/images/category/664613f57fe0f.png",
+      cover: "https://4kwallpapers.com/images/walls/thumbs_2t/19423.jpg",
       redirect: "https://imgur.com/a/6L5s8Z1"
     },
+
+    // ash
     {
-      object3D: "https://i.imgur.com/m2M60Bj.png",
-      title: "https://i.imgur.com/DHZTe9G.png",
-      cover: "https://i.imgur.com/4DyAosb.jpeg",
+      object3D: "https://i.imgur.com/RJZODdb.gif",
+      title: "https://logodownload.org/wp-content/uploads/2017/08/pokemon-logo-8.png",
+      cover: "https://i.pinimg.com/736x/d6/b9/b2/d6b9b229742fa2ab8c620173953bef8b.jpg",
       redirect: "https://imgur.com/a/6L5s8Z1"
     }
 ]
 ```
+
+Github repository link: https://github.com/CaioDiogenes/3D-Card-hover-for-gallery
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,15 +15,117 @@ export class AppComponent {
   imgs: Array<any> = [
     {
       object3D: "https://i.imgur.com/SD9RHH1.png",
-      title: "https://occ-0-8407-92.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABWJRhnrz30Sbs7lCtAIwDK32PEalrSKLuVcA3NjVXvfksVF0pnMo6xF3b5jgvsCa2sWdPx9uW9qdq5DiLURdlMRd41oAv9lueQSexRP_rmB2.png?r=420",
+      title: "https://upload.wikimedia.org/wikipedia/commons/b/b2/Dandadan_logo.png",
       cover: "https://images-ext-1.discordapp.net/external/fYy773moensHQgRyLjGsP86EtlnzDc6IMUDM2gKTFn8/%3Fwidth%3D857%26height%3D1200%26hash%3D2057/https/ae01.alicdn.com/kf/S41ee5c11910540c0857553d42abf3610P.jpg?format=webp&width=479&height=671",
       redirect: "https://imgur.com/a/6L5s8Z1"
     },
+
+    // okarun
     {
-      object3D: "https://i.imgur.com/m2M60Bj.png",
-      title: "https://i.imgur.com/DHZTe9G.png",
-      cover: "https://i.imgur.com/4DyAosb.jpeg",
+      object3D: "https://i.imgur.com/vWa25XP.gif",
+      title: "https://tamashiiweb.com/images/category/664613f57fe0f.png",
+      cover: "https://4kwallpapers.com/images/walls/thumbs_2t/19423.jpg",
       redirect: "https://imgur.com/a/6L5s8Z1"
-    }
+    },
+
+    // ash
+    {
+      object3D: "https://i.imgur.com/RJZODdb.gif",
+      title: "https://logodownload.org/wp-content/uploads/2017/08/pokemon-logo-8.png",
+      cover: "https://i.pinimg.com/736x/d6/b9/b2/d6b9b229742fa2ab8c620173953bef8b.jpg",
+      redirect: "https://imgur.com/a/6L5s8Z1"
+    },
+    
+    {
+      object3D: "https://i.imgur.com/vWa25XP.gif",
+      title: "https://tamashiiweb.com/images/category/664613f57fe0f.png",
+      cover: "https://4kwallpapers.com/images/walls/thumbs_2t/19423.jpg",
+      redirect: "https://imgur.com/a/6L5s8Z1"
+    },{
+      object3D: "https://i.imgur.com/vWa25XP.gif",
+      title: "https://tamashiiweb.com/images/category/664613f57fe0f.png",
+      cover: "https://4kwallpapers.com/images/walls/thumbs_2t/19423.jpg",
+      redirect: "https://imgur.com/a/6L5s8Z1"
+    },{
+      object3D: "https://i.imgur.com/vWa25XP.gif",
+      title: "https://tamashiiweb.com/images/category/664613f57fe0f.png",
+      cover: "https://4kwallpapers.com/images/walls/thumbs_2t/19423.jpg",
+      redirect: "https://imgur.com/a/6L5s8Z1"
+    },{
+      object3D: "https://i.imgur.com/vWa25XP.gif",
+      title: "https://tamashiiweb.com/images/category/664613f57fe0f.png",
+      cover: "https://4kwallpapers.com/images/walls/thumbs_2t/19423.jpg",
+      redirect: "https://imgur.com/a/6L5s8Z1"
+    },{
+      object3D: "https://i.imgur.com/vWa25XP.gif",
+      title: "https://tamashiiweb.com/images/category/664613f57fe0f.png",
+      cover: "https://4kwallpapers.com/images/walls/thumbs_2t/19423.jpg",
+      redirect: "https://imgur.com/a/6L5s8Z1"
+    },{
+      object3D: "https://i.imgur.com/vWa25XP.gif",
+      title: "https://tamashiiweb.com/images/category/664613f57fe0f.png",
+      cover: "https://4kwallpapers.com/images/walls/thumbs_2t/19423.jpg",
+      redirect: "https://imgur.com/a/6L5s8Z1"
+    },{
+      object3D: "https://i.imgur.com/vWa25XP.gif",
+      title: "https://tamashiiweb.com/images/category/664613f57fe0f.png",
+      cover: "https://4kwallpapers.com/images/walls/thumbs_2t/19423.jpg",
+      redirect: "https://imgur.com/a/6L5s8Z1"
+    },{
+      object3D: "https://i.imgur.com/vWa25XP.gif",
+      title: "https://tamashiiweb.com/images/category/664613f57fe0f.png",
+      cover: "https://4kwallpapers.com/images/walls/thumbs_2t/19423.jpg",
+      redirect: "https://imgur.com/a/6L5s8Z1"
+    },{
+      object3D: "https://i.imgur.com/vWa25XP.gif",
+      title: "https://tamashiiweb.com/images/category/664613f57fe0f.png",
+      cover: "https://4kwallpapers.com/images/walls/thumbs_2t/19423.jpg",
+      redirect: "https://imgur.com/a/6L5s8Z1"
+    },{
+      object3D: "https://i.imgur.com/vWa25XP.gif",
+      title: "https://tamashiiweb.com/images/category/664613f57fe0f.png",
+      cover: "https://4kwallpapers.com/images/walls/thumbs_2t/19423.jpg",
+      redirect: "https://imgur.com/a/6L5s8Z1"
+    },{
+      object3D: "https://i.imgur.com/vWa25XP.gif",
+      title: "https://tamashiiweb.com/images/category/664613f57fe0f.png",
+      cover: "https://4kwallpapers.com/images/walls/thumbs_2t/19423.jpg",
+      redirect: "https://imgur.com/a/6L5s8Z1"
+    },{
+      object3D: "https://i.imgur.com/vWa25XP.gif",
+      title: "https://tamashiiweb.com/images/category/664613f57fe0f.png",
+      cover: "https://4kwallpapers.com/images/walls/thumbs_2t/19423.jpg",
+      redirect: "https://imgur.com/a/6L5s8Z1"
+    },{
+      object3D: "https://i.imgur.com/vWa25XP.gif",
+      title: "https://tamashiiweb.com/images/category/664613f57fe0f.png",
+      cover: "https://4kwallpapers.com/images/walls/thumbs_2t/19423.jpg",
+      redirect: "https://imgur.com/a/6L5s8Z1"
+    },{
+      object3D: "https://i.imgur.com/vWa25XP.gif",
+      title: "https://tamashiiweb.com/images/category/664613f57fe0f.png",
+      cover: "https://4kwallpapers.com/images/walls/thumbs_2t/19423.jpg",
+      redirect: "https://imgur.com/a/6L5s8Z1"
+    },{
+      object3D: "https://i.imgur.com/vWa25XP.gif",
+      title: "https://tamashiiweb.com/images/category/664613f57fe0f.png",
+      cover: "https://4kwallpapers.com/images/walls/thumbs_2t/19423.jpg",
+      redirect: "https://imgur.com/a/6L5s8Z1"
+    },{
+      object3D: "https://i.imgur.com/vWa25XP.gif",
+      title: "https://tamashiiweb.com/images/category/664613f57fe0f.png",
+      cover: "https://4kwallpapers.com/images/walls/thumbs_2t/19423.jpg",
+      redirect: "https://imgur.com/a/6L5s8Z1"
+    },{
+      object3D: "https://i.imgur.com/vWa25XP.gif",
+      title: "https://tamashiiweb.com/images/category/664613f57fe0f.png",
+      cover: "https://4kwallpapers.com/images/walls/thumbs_2t/19423.jpg",
+      redirect: "https://imgur.com/a/6L5s8Z1"
+    },{
+      object3D: "https://i.imgur.com/vWa25XP.gif",
+      title: "https://tamashiiweb.com/images/category/664613f57fe0f.png",
+      cover: "https://4kwallpapers.com/images/walls/thumbs_2t/19423.jpg",
+      redirect: "https://imgur.com/a/6L5s8Z1"
+    },
   ]
 }
